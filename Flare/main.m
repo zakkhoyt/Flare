a7c75f99e75cb7230edc3f4e8f5fbd22601836a5
Zakk Hoyt <vaporwarewolf@gmail.com>
2013-10-17 20:15:20 -0700

    Initial Commit

---
+++ Flare/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Flare
+//
+//  Created by Zakk Hoyt on 10/17/13.
+//  Copyright (c) 2013 Zakk Hoyt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
